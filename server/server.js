00e73131b2b3d98cda4056b58dbad3a95f759b42
febriadj <iamfebriadji@gmail.com>
2021-08-27 13:09:22 +0700

    enable cors request

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema/index');
 const dbConnect = require('./dbConnect');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
+dbConnect();
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
-
-dbConnect();
 
 app.listen(port);
 console.log(`Server running on port ${port}`);
